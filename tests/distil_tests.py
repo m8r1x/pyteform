bcf75d4ceef2e597b70a497d9ea9da4c2d284b0e
m8r1x <m8r1x.io@gmail.com>
2017-09-27 07:59:33 +0300

    refactor test descriptions

--- tests/distil_tests.py
+++ tests/distil_tests.py
@@ -20,29 +20,29 @@ def load_answers(file):
 class TestDistil(unittest.TestCase):
 	""" Tests for `distil.py`. """
 	def test_filter_emails(self):
-		""" email is extracted using regex """
+		""" it should extract emails using regex """
 		text = "I can extract your email@provider.domain from this text."
 		email = filter_emails(text)
 		assert "email@provider.domain" == email
 
 	def test_email(self):
-		""" returns dict = { 'field_id': ['email', ...] } """
+		""" it should return { 'field_id': ['email', ...] } """
 		df = load_answers("mockapi/v1/form/TYPEFORM_ID.json")
 		emails = email(df)
 		assert emails['email_37962684'][0] == "test@typeform.com"
 
 	def test_file_upload_url(self):
-		""" returns dict = { 'field_id': ['file_upload_url', ...] } """
+		""" it should return { 'field_id': ['file_upload_url', ...] } """
 		df = load_answers("mockapi/v1/form/TYPEFORM_ID.json")
 		file_upload_urls = file_upload_url(df)
 		assert "Cersei.jpg" in file_upload_urls['fileupload_37980234'][0]
 
 	def test_email_throws(self):
-		""" it should throw a TypeError """
+		""" it should throw TypeError given non (list or dataframe) params """
 		self.assertRaises(TypeError, email, {})
 
 	def test_file_upload_throws(self):
-		""" it should throw a TypeError """
+		""" it should throw TypeError given non (list or dataframe) params """
 		self.assertRaises(TypeError, file_upload_url, {})
 
 if __name__ == '__main__':
