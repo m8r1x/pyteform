acfacd1dfa7f36f1fddebd13a71deb9affa70f0b
m8r1x <m8r1x.io@gmail.com>
2017-09-28 08:31:29 +0300

    add authors info, description and exclude tests

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
 	name='pyteform',
 	version='0.1.0-alpha',
-	packages=find_packages(),
+	author="m8r1x",
+	author_email="m8r1x.io@gmail.com",
+	description="A python wrapper for the typeform api.",
+	packages=find_packages(exclude=["*.tests", "*.tests.", "tests.*", "tests"]),
 	license='MIT',
 	install_requires=[
 		'pandas'
